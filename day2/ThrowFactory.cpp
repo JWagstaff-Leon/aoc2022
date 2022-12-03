6f66c2e2305a9abf5cecca5b640548b7146c6fac
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-12-02 23:52:56 -0700

    updated for part2

--- day2/ThrowFactory.cpp
+++ day2/ThrowFactory.cpp
@@ -38,3 +38,94 @@ std::unique_ptr<Throw> ThrowFactory::makeThrowFromCode(char throwCode)
     };
     return madeThrow;
 };
+
+
+
+std::unique_ptr<Throw> ThrowFactory::makeOutcomeFromCode(char outcomeCode, ThrowType contestingThrow)
+{
+    std::unique_ptr<Throw> madeThrow = std::make_unique<InvalidThrow>();
+    switch (outcomeCode)
+    {
+        case 'X':
+            madeThrow = makeLosingThrow(contestingThrow);
+            break;
+        case 'Y':
+            madeThrow = makeDrawThrow(contestingThrow);
+            break;
+        case 'Z':
+            madeThrow = makeWinningThrow(contestingThrow);
+            break;
+        default:
+            break;
+    };
+    return madeThrow;
+};
+
+
+
+std::unique_ptr<Throw> ThrowFactory::makeLosingThrow(ThrowType contestingThrow)
+{
+    std::unique_ptr<Throw> losingThrow = std::make_unique<InvalidThrow>();
+    switch (contestingThrow)
+    {
+        case ThrowType::Rock:
+            losingThrow = std::make_unique<ScissorsThrow>();
+            break;
+        case ThrowType::Paper:
+            losingThrow = std::make_unique<RockThrow>();
+            break;
+        case ThrowType::Scissors:
+            losingThrow = std::make_unique<PaperThrow>();
+            break;
+        default:
+            losingThrow = std::make_unique<InvalidThrow>();
+            break;
+    };
+    return losingThrow;
+};
+
+
+
+std::unique_ptr<Throw> ThrowFactory::makeDrawThrow(ThrowType contestingThrow)
+{
+    std::unique_ptr<Throw> drawingThrow = std::make_unique<InvalidThrow>();
+    switch (contestingThrow)
+    {
+        case ThrowType::Rock:
+            drawingThrow = std::make_unique<RockThrow>();
+            break;
+        case ThrowType::Paper:
+            drawingThrow = std::make_unique<PaperThrow>();
+            break;
+        case ThrowType::Scissors:
+            drawingThrow = std::make_unique<ScissorsThrow>();
+            break;
+        default:
+            drawingThrow = std::make_unique<InvalidThrow>();
+            break;
+    };
+    return drawingThrow;
+};
+
+
+
+std::unique_ptr<Throw> ThrowFactory::makeWinningThrow(ThrowType contestingThrow)
+{
+    std::unique_ptr<Throw> winningThrow = std::make_unique<InvalidThrow>();
+    switch (contestingThrow)
+    {
+        case ThrowType::Rock:
+            winningThrow = std::make_unique<PaperThrow>();
+            break;
+        case ThrowType::Paper:
+            winningThrow = std::make_unique<ScissorsThrow>();
+            break;
+        case ThrowType::Scissors:
+            winningThrow = std::make_unique<RockThrow>();
+            break;
+        default:
+            winningThrow = std::make_unique<InvalidThrow>();
+            break;
+    };
+    return winningThrow;
+};
--- day2/ThrowFactory.h
+++ day2/ThrowFactory.h
@@ -10,6 +10,12 @@ class ThrowFactory
     public:
         static std::unique_ptr<Throw> makeThrowFromCode(char throwCode);
 
+        static std::unique_ptr<Throw> makeOutcomeFromCode(char outcomeCode, ThrowType contestingThrow);
+
+        static std::unique_ptr<Throw> makeLosingThrow (ThrowType contestingThrow);
+        static std::unique_ptr<Throw> makeDrawThrow   (ThrowType contestingThrow);
+        static std::unique_ptr<Throw> makeWinningThrow(ThrowType contestingThrow);
+
     private:
         ThrowFactory();
 };
