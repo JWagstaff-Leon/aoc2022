e09a9e734169faacd02677d185321549572f5d5a
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2023-11-27 11:27:45 -0700

    part 2

---
+++ day13/part2.cpp
@@ -0,0 +1,118 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Packet.h"
+
+ListPacket* stringToList(std::string string)
+{
+    ListPacket* outList = new ListPacket;
+    for(int i = 1; i < string.length() - 1; i++)
+    {
+        if(string[i] == '[')
+        {
+            std::string substring;
+            int j, depth;
+            for(
+                j = i + 1, depth = 1;
+                j < string.length() - 1 && depth > 0;
+                j++
+            )
+            {
+                if(string[j] == '[')
+                {
+                    depth += 1;
+                }
+                if(string[j] == ']')
+                {
+                    depth -= 1;
+                }
+            }
+            substring = string.substr(i, j - i); // start after '['; end before ']'
+            outList->addData(static_cast<Packet*>(stringToList(substring)));
+        }
+
+        if(string[i] >= '0' && string[i] <= '9')
+        {
+            int value = 0;
+            for(/* no init */; string[i] >= '0' && string[i] <= '9' && i < string.length(); i++)
+            {
+                value *= 10;
+                value += string[i] - '0';
+            }
+
+            IntegerPacket* valuePacket = new IntegerPacket(value);
+            outList->addData(static_cast<Packet*>(valuePacket));
+        }
+    }
+
+    return outList;
+};
+
+
+
+struct {
+    bool operator() (ListPacket* lhs, ListPacket* rhs) { return packetLessThan(lhs, rhs); };
+} ListPacketComp;
+
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <input filename>" << std::endl;
+        return 0;
+    }
+
+    std::ifstream fin(argv[1]);
+    if (fin.fail())
+    {
+        std::cout << "Could not open file " << argv[1] << std::endl;
+        return 0;
+    }
+
+
+    std::vector<ListPacket*> packets;
+    while(!fin.eof())
+    {
+        std::string list1String, list2String;
+        std::getline(fin, list1String);
+        std::getline(fin, list2String);
+
+        ListPacket* list1 = stringToList(list1String);
+        ListPacket* list2 = stringToList(list2String);
+        packets.push_back(list1);
+        packets.push_back(list2);
+        
+        std::getline(fin, list1String); // consume blank line
+    }
+    fin.close();
+
+    ListPacket* divider1 = stringToList("[[2]]");
+    ListPacket* divider2 = stringToList("[[6]]");
+    packets.push_back(divider1);
+    packets.push_back(divider2);
+
+    std::sort(packets.begin(), packets.end(), ListPacketComp);
+    
+    int answer = 1;
+    for(int i = 0; i < packets.size(); i++)
+    {
+        if(packets[i] == divider1 || packets[i] == divider2)
+        {
+            answer *= i + 1;
+        }
+    }
+
+    std::cout << "Answer: " << answer << std::endl;
+
+    for(auto it = packets.rbegin(); it != packets.rend(); it++)
+    {
+        delete *it;
+    }
+
+    return 0;
+}
